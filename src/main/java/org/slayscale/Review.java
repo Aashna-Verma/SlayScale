13c7daffab331d47138afde417f2fa379b0243bf
Hubert Dang <hubertdang@gmail.com>
2025-10-28 18:25:31 -0400

    Create Review class and test with missing product and author fields

---
+++ src/main/java/org/slayscale/Review.java
@@ -0,0 +1,54 @@
+package org.slayscale;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    // TODO: put Product class field here for 'product'
+    // TODO: put User class field here for 'author'
+    int rating; // 0 - 5 stars
+    String text;
+
+    protected Review() {}
+
+    public Review(int rating, String text) {
+        setRating(rating);
+        setText(text);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        if (rating < 0 || rating > 5) {
+            throw new IllegalArgumentException("Rating must be between 0 and 5.");
+        }
+        this.rating = rating;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        if (text == null) {
+            throw new IllegalArgumentException("Text cannot be null.");
+        }
+        this.text = text;
+    }
+}
