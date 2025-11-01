f3f6aa65b4184accd2f3370dac8829099b6bc09a
Amilesh Nanthakumaran <62160154+AmileshN@users.noreply.github.com>
2025-11-01 10:09:54 -0400

    9 implement product class (#25)
    * 9-implement-product-class
    
    * Delegate setting the product of a review to the controller
    
    * Fixed review tests
    
    * Fixed a product test
    
    * Fixed test names, added getsetproduct test and spacing issues
    
    * Refactored product test

---
+++ src/test/java/org/slayscale/ProductTest.java
@@ -0,0 +1,67 @@
+package org.slayscale;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class ProductTest {
+    private Product product;
+    private Review review;
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        product = new Product(Category.ELECTRONICS, "http://costco.ca/led-patel-signs/p-24");
+        user = new User("rajesh");
+        review = new Review(user, 4, "vhat a beautiful sign", product);
+    }
+
+
+    @Test
+    public void getSetId() {
+        product.setId(10L);
+        assert (product.getId() == 10L);
+    }
+
+    @Test
+    public void getSetCategory() {
+        assert (product.getCategory() == Category.ELECTRONICS);
+        product.setCategory(Category.BOOKS);
+        assert (product.getCategory() == Category.BOOKS);
+        assertThrows(IllegalArgumentException.class, () -> product.setCategory(null));
+    }
+
+    @Test
+    public void setUrl() {
+        assert (product.getUrl().equals("http://costco.ca/led-patel-signs/p-24"));
+        product.setUrl("http://amazon.ca/kindle/p-99");
+        assert (product.getUrl().equals("http://amazon.ca/kindle/p-99"));
+        assertThrows(IllegalArgumentException.class, () -> product.setUrl(" "));
+    }
+
+    @Test
+    public void getSetReviews() {
+        assert (product.getReviews().isEmpty());
+        product.getReviews().add(review);
+        assert (product.getReviews().size() == 1);
+        assertThrows(IllegalArgumentException.class, () -> product.setReviews(null));
+    }
+
+    @Test
+    public void addReview() {
+        product.addReview(review);
+        assert (product.getReviews().size() == 1);
+        assert (review.getProduct() == product);
+        assertThrows(IllegalArgumentException.class, () -> product.addReview(null));
+    }
+
+    @Test
+    public void removeReview() {
+        product.addReview(review);
+        product.removeReview(review);
+        assert (product.getReviews().isEmpty());
+        assertThrows(IllegalArgumentException.class, () -> product.removeReview(null));
+    }
+
+}
