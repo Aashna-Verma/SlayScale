3ba03de77f56e16587d3820c6a8812445e4d5149
AmileshN <amilesh2003@gmail.com>
2025-11-15 08:24:34 -0500

    Updated method names to seedData

--- src/main/java/org/slayscale/LambdaService.java
+++ src/main/java/org/slayscale/LambdaService.java
@@ -1,7 +1,5 @@
 package org.slayscale;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.lambda.LambdaClient;
@@ -17,10 +15,8 @@ public class LambdaService {
         this.lambdaClient = lambdaClient;
     }
 
-    public String getSimilarity(User u1, User u2) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        String payload = mapper.writeValueAsString(u1) + "," + mapper.writeValueAsString(u2);
-
+    public String seedData() {
+        String payload = "";
         InvokeRequest request = InvokeRequest.builder()
                 .functionName("hellojava")  // Lambda name
                 .payload(SdkBytes.fromUtf8String(payload))
